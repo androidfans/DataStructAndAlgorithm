3bf8b5fd0190cd6a971bc5bb65aaa7c158f0b2ac
androidfans <2458853753@qq.com>
2016-01-12 10:32:01 +0800

    complete Stack

---
+++ Stack.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <cassert>
+using namespace std;
+
+template <class T>
+class Stack {
+private:
+	int capacity;
+	int top;
+	T *data;
+public:
+	Stack(int _capacity) : top(-1),capacity(_capacity) {
+		data = new T[_capacity];
+	}
+	bool push(T element) {
+		if (top >= capacity - 1) {
+			return false;
+		}
+		top++;
+		data[top] = element;
+		return true;
+	}
+
+	T top() {
+		assert(top > -1);
+		return data[top];
+	}
+
+	bool pop() {
+		if (top < 0) {
+			return false;
+		}
+		top--;
+		return true;
+	}
+};
+
+int operate(char theta, int a, int b) {
+	if (theta == '+') {
+		return a + b;
+	}
+	else
+	{
+		return a * b;
+	}
+}
+
+
+int main() {
+	Stack<int> stack(200);
+
+
+
+
+
+	return 0;
+}
